ecbe56b67559603d5f860b135e34b7d9c7570598
eduardo <eduregis1@gmail.com>
2019-12-19 14:27:24 -0300

    adicionando dependecias do projeto

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <TheNavbar/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar.vue'
+export default {
+  components: {
+    TheNavbar
+  }  
+}
+</script>
+
+<style>
+  
+</style>
